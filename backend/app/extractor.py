244644745f56f9d1b6cdb74c66ed73e7c4bdb5f0
talh <talhaim.dev@gmail.com>
2025-02-27 14:31:18 +0200

    develop: fix extractor fields: ips, status code, tech.

--- backend/app/extractor.py
+++ backend/app/extractor.py
@@ -15,13 +15,24 @@ class HTTPXExtractor(BaseExtractor):
     Extracts and maps fields from HTTPX output.
     """
     def extract(self, raw_data: Dict[str, Any]) -> Dict[str, Any]:
+        """
+
+        :param raw_data:
+        :return:
+        """
+        logger.info(f"Raw Data: {raw_data}")
+
+        ipv4 = raw_data.get("a", [])
+        ipv6 = raw_data.get("aaaa", [])
+        related_ips = list(set(ipv4 + ipv6))
+
         extracted = {
             "domain": raw_data.get("input", ""),
-            "related_ips": raw_data.get("ips", []),
+            "related_ips": related_ips,
             "webpage_title": raw_data.get("title", ""),
-            "status_code": raw_data.get("status-code", None),
+            "status_code": raw_data.get("status_code", None),
             "webserver": raw_data.get("webserver", ""),
-            "technologies": raw_data.get("technologies", []),
+            "technologies": raw_data.get("tech", []),
             "cnames": raw_data.get("cnames", [])
         }
         logger.info(f"Extracted Data: {extracted}")
