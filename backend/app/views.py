30afd1d348d2be4cb1705ec12bec6f921d77e2b9
talh <talhaim.dev@gmail.com>
2025-02-27 18:44:55 +0200

    develop: views: root, /home routes

---
+++ backend/app/views.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="static/templates")
+
+@router.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return RedirectResponse(url="/home")
+
+@router.get("/home", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
